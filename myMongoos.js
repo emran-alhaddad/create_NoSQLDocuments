e35dc8a60a627453165bf2197c9639b27ec127b9
emranco <emranhadad770774255770774255@gmail.com>
2022-02-12 00:05:53 +0300

    init

--- myMongoos.js
+++ myMongoos.js
@@ -1,32 +1,37 @@
 var Book = require('./BookModule');
 
-console.log(new Date(), 'Inserting doc');
 Book.create({ title: "C++", author: "EmranCo", rating: 4 }, function(err) {
     if (err) return handleError(err);
-    console.log("Inserted Success");
+    console.log(new Date().toLocaleString(), "Inserted Success");
 });
 
-console.log(new Date(), 'Inserting doc');
 Book.create({ title: "C#", author: "Haifa", rating: 5 }, function(err) {
     if (err) return handleError(err);
-    console.log("Inserted Success");
+    console.log(new Date().toLocaleString(), "Inserted Success");
 });
 
-console.log(new Date(), 'Inserting doc');
 Book.create({ title: "Bootstrab5", author: "Moghtar", rating: 5 }, function(err) {
     if (err) return handleError(err);
-    console.log("Inserted Success");
-});
-
-console.log(new Date(), 'Updating doc');
-Book.updateOne({ title: "C++" }, { $set: { author: "Emran Ali", rating: 5 } }, function(err) {
-    if (err) return handleError(err);
-    console.log("Updated Success");
+    console.log(new Date().toLocaleString(), "Inserted Success");
 });
 
 
-console.log(new Date(), 'Deleting doc');
-Book.deleteOne({ author: "Moghtar" }, function(err) {
+Book.updateMany({ title: "C++" }, { author: "Emran Ali", rating: 5 }, { returnDocument: 'after' }, function(err) {
     if (err) return handleError(err);
-    console.log("Deleting Success");
+    console.log(new Date().toLocaleString(), "Updated Success");
+});
+
+
+Book.remove({ author: "Moghtar" }, { returnDocument: 'after' }, function(err) {
+    if (err) return handleError(err);
+    console.log(new Date().toLocaleString(), "Deleting Success");
+});
+
+
+
+Book.find((err, data) => {
+    if (err) console.log(err);
+    else console.log(new Date().toLocaleString(), data.forEach((e) => {
+        console.log(e, "\n---------------\n");
+    }));
 });
